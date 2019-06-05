216979d1f1e4097708dff7f45b83623d74550ed0
Jake Rowe <39175090+JakeRDesign@users.noreply.github.com>
2019-06-05 15:22:11 +1000

    Level layout relatively fixed.

---
+++ Shaders for the Blind/Assets/Scripts/TestMov.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMov : MonoBehaviour
+{
+    public float playerSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.position += Vector3.forward * playerSpeed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.position += Vector3.back * playerSpeed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.position += Vector3.left * playerSpeed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.position += Vector3.right * playerSpeed * Time.deltaTime;
+            }
+        }
+    }
+}
