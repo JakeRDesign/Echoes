4bc2b45813d2fd4bbf6473fa336d841f3949b9e3
Jake Rowe <39175090+JakeRDesign@users.noreply.github.com>
2019-06-06 14:00:12 +1000

    Edited blood trail.

--- Shaders for the Blind/Assets/Scripts/BloodSpawn.cs
+++ Shaders for the Blind/Assets/Scripts/BloodSpawn.cs
@@ -22,12 +22,46 @@ public class BloodSpawn : MonoBehaviour
             transform.position = transform.position + new Vector3(.1f, 0, 0);
         }
 
+        if (Input.GetKey(KeyCode.S))
+        {
+            moving = true;
+            transform.position = transform.position + new Vector3(-.1f, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            moving = true;
+            transform.position = transform.position + new Vector3(0, 0, .1f);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            moving = true;
+            transform.position = transform.position + new Vector3(0, 0, -.1f);
+        }
+
         if (moving == true)
         {
             Instantiate(blood[Random.Range(0, blood.Length)], player.position, player.rotation);
+            new WaitForSeconds(2f);
         }
 
         if (Input.GetKeyUp(KeyCode.W))
+        {
+            moving = false;
+        }
+
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            moving = false;
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            moving = false;
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
         {
             moving = false;
         }
