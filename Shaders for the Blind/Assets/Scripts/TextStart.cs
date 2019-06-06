f0025fbea8753d21e476c5f77c022601313e854e
Jake Rowe <39175090+JakeRDesign@users.noreply.github.com>
2019-06-06 16:06:04 +1000

    Start scene with credits.

---
+++ Shaders for the Blind/Assets/Scripts/TextStart.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextStart : MonoBehaviour
+{
+    public GameObject thing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thing.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            thing.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            thing.gameObject.SetActive(false);
+        }
+    }
+}
