1ce6ba52bca8ca22494309102a45254ad91756d3
Cameron Brown <c@meronbrown.com>
2019-06-06 12:24:54 +1000

    beginning of enemy behaviour

---
+++ Shaders for the Blind/Assets/Editor/EnemyEditor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(EnemyController))]
+public class EnemyEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        EnemyController e = target as EnemyController;
+        if (e == null)
+            return;
+
+        if (e.pathPoints.Count == 0)
+            return;
+
+        Handles.color = Color.cyan;
+        Vector3 lastPos = e.pathPoints[0].position;
+        for (int i = 0; i < e.pathPoints.Count; ++i)
+        {
+            PathPoint pt = e.pathPoints[i];
+            pt.position = Handles.PositionHandle(pt.position, Quaternion.identity);
+
+            Handles.DrawLine(lastPos, pt.position);
+            lastPos = pt.position;
+            e.pathPoints[i] = pt;
+        }
+        Handles.DrawLine(lastPos, e.pathPoints[0].position);
+    }
+}
---
+++ Shaders for the Blind/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct PathPoint
+{
+    public Vector3 position;
+    public float pauseTime;
+}
+
+public class EnemyController : MonoBehaviour
+{
+
+    public List<PathPoint> pathPoints;
+    public float moveSpeed = 10.0f;
+
+    int pathIndex = 0;
+    float pauseTimer = 0.0f;
+
+    private void FixedUpdate()
+    {
+        if (pathPoints.Count == 0)
+            return;
+
+        if(pauseTimer > 0.0f)
+        {
+            pauseTimer -= Time.fixedDeltaTime;
+            return;
+        }
+
+        PathPoint thisPoint = pathPoints[pathIndex];
+
+        Vector3 difference = thisPoint.position - transform.position;
+        float mag = difference.magnitude;
+
+        if(mag < 0.1f)
+        {
+            pathIndex = (pathIndex + 1) % pathPoints.Count;
+            pauseTimer = thisPoint.pauseTime;
+            return;
+        }
+
+        difference.Normalize();
+
+        Vector3 movement = difference * moveSpeed;
+        transform.position += movement * Time.fixedDeltaTime;
+    }
+
+
+}
