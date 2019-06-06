a70a5a69830db3105362c317aea0206de78fe1b4
Jake Rowe <39175090+JakeRDesign@users.noreply.github.com>
2019-06-06 12:31:52 +1000

    Blood trail. WIP.

---
+++ Shaders for the Blind/Assets/Scripts/BloodSpawn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodSpawn : MonoBehaviour
+{
+    public GameObject[] blood;
+    private bool moving = false;
+
+    public Transform player;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            moving = true;
+            transform.position = transform.position + new Vector3(.1f, 0, 0);
+        }
+
+        if (moving == true)
+        {
+            Instantiate(blood[Random.Range(0, blood.Length)], player.position, player.rotation);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            moving = false;
+        }
+    }
+}
