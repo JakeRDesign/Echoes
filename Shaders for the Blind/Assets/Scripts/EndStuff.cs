a6570e53232817263a43c3e6408fc9f4604c9602
Jake Rowe <39175090+JakeRDesign@users.noreply.github.com>
2019-06-13 12:16:51 +1000

    WIP end scene. Text meshes.

---
+++ Shaders for the Blind/Assets/Scripts/EndStuff.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndStuff : MonoBehaviour
+{
+    public GameObject colliders;
+    public GameObject stage;
+
+    public GameObject text;
+    public GameObject light;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        colliders.gameObject.SetActive(false);
+        text.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            stage.gameObject.SetActive(false);
+            text.gameObject.SetActive(true);
+            colliders.gameObject.SetActive(true);
+            light.gameObject.SetActive(false);
+        }
+    }
+}
