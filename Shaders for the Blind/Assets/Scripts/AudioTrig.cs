1b40ae1899c7a436d4dbe2a24a74fb73fb5de858
Jake Rowe <39175090+JakeRDesign@users.noreply.github.com>
2019-06-13 10:58:16 +1000

    Audio framework and trigger for end scene.

---
+++ Shaders for the Blind/Assets/Scripts/AudioTrig.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTrig : MonoBehaviour
+{
+    public AudioSource screech;
+    private bool firsttime = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (firsttime == false)
+            {
+                screech.Play();
+                firsttime = true;
+            }
+        }
+    }
+}
