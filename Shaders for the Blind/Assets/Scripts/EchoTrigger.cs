39e468af7703d7f994db9807fb80e740eb634f8f
Cameron Brown <c@meronbrown.com>
2019-06-19 09:10:13 +1000

    echo can start slower and speed up over time

--- Shaders for the Blind/Assets/Scripts/EchoTrigger.cs
+++ Shaders for the Blind/Assets/Scripts/EchoTrigger.cs
@@ -7,15 +7,20 @@ using UnityEditor;
 public class EchoTrigger : MonoBehaviour
 {
 
+    public float echoAcceleration = 1.0f;
     public float echoSpeed = 1.0f;
 
     public float echoDist = 0.0f;
     float lastDist = 0.0f;
 
+    float currentSpeed = 0.0f;
+
     List<EnemyPulse> pulses;
 
     private void Awake()
     {
+        currentSpeed = echoSpeed / 2.0f;
+
         pulses = new List<EnemyPulse>();
 
         foreach (var p in FindObjectsOfType<EnemyPulse>())
@@ -25,7 +30,13 @@ public class EchoTrigger : MonoBehaviour
     void Update()
     {
         if (Application.isPlaying)
-            echoDist += Time.deltaTime * echoSpeed;
+        {
+            if (currentSpeed < echoSpeed)
+                currentSpeed += Time.deltaTime * echoAcceleration;
+            if (currentSpeed > echoSpeed)
+                currentSpeed = echoSpeed;
+            echoDist += Time.deltaTime * currentSpeed;
+        }
 
         Shader.SetGlobalFloat("_EchoRadius", echoDist);
 
