60595d16e7444c464f685ca9eb995f2323e8255e
Jake Rowe <39175090+JakeRDesign@users.noreply.github.com>
2019-06-19 12:58:13 +1000

    Controls text in start scene.

---
+++ Shaders for the Blind/Assets/Scripts/ControlsTrig.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlsTrig : MonoBehaviour
+{
+    public GameObject controls;
+    public GameObject text;
+    private bool firsttime;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if ((other.tag == "Player") && firsttime == false)
+        {
+            controls.transform.position = controls.transform.position + new Vector3(0, 10f, 0);
+            text.transform.position = controls.transform.position + new Vector3(0, 10f, 0);
+            firsttime = true;
+        }
+    }
+}
